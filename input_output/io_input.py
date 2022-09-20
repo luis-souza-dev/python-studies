eed4d90b47ddb8dffb0bb391807ec99f815e28e8
Luis Souza <luisotavioldesouza1+1@gmail.com>
2022-09-20 13:13:36 -0300

    check palindrome algorithim now working

--- input_output/io_input.py
+++ input_output/io_input.py
@@ -1,28 +1,16 @@
 import string
 
-def check_symbols(str):
-    forbidden = ('!',',','.',':',';','\\','/','*',' ')
-    forbidden_index = -1
-    check_str = str[:]
-
-    for symbol in forbidden:
-        forbidden_index = check_str.find(symbol)
-    if forbidden_index > -1:
-        print('check_str', check_str)
-        print('fi', forbidden_index)
-        print('str[0 : forbidden_index : ]', str[0 : forbidden_index : ])
-        print('str[forbidden_index + 1 : :]', str[forbidden_index + 1 : :])
-        check_str = str[0 : forbidden_index : ] + str[forbidden_index + 1 : :]
-    return check_str
+def remove_symbols(input):
+    return input.lower().translate(str.maketrans('','', string.punctuation)).replace(' ','')
 
 def reverse(something):
-    return something.translate(str.maketrans('','',string.punctuation))[::-1]
+    return something[::-1]
 
 def is_palindrome(something):
-    return something.translate(str.maketrans('','',string.punctuation)) == reverse(something)
+    return something == reverse(something)
 
 something = input('Enter something: ')
-if is_palindrome(something):
-    print('Yes, it is palindrome', something, ' ', reverse(something))
+if is_palindrome(remove_symbols(something)):
+    print('Yes, it is palindrome', remove_symbols(something), ' ', reverse(something))
 else:
-    print('No, it isn\'t palindrome', something.translate(str.maketrans('','',string.punctuation)), ' ', reverse(something))
+    print('No, it isn\'t palindrome', something.translate(str.maketrans('','',string.punctuation)).replace(' ','').lower(), ' ', reverse(something))
